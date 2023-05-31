51d1b2ceb6ff86bc7626fe218f136a2e9b11a06e
RuthvikMukkamala <ruthvikmukkamala@gmail.com>
2023-05-30 21:52:01 -0400

    create website

---
+++ constants/data.js
@@ -0,0 +1,76 @@
+const userData = {
+  githubUsername: "ruthvikmukkamala",
+  name: "Ruthvik Mukkamala",
+  designation: "Founder and CEO of Roebling",
+  avatarUrl: "/avatar.png",
+  email: "ruthvikmukkamala@nyu.edu",
+  phone: "+1 609 233 4478",
+  address: "8 Sleepy Hollow Lane, Princeton Junction, New Jersey",
+  projects: [
+    {
+      title: "xballtracker",
+      link: "",
+      imgUrl: "/tmk.jpg",
+    },
+  ],
+  about: {
+    title:
+      "I'm a current student at New York University Tandon School of Engineering studying Applied Mathematics and Computer Science",
+    description: [
+      `My name is Ruthvik Mukkamala.`,
+      `I have always been enchanted with human innovation - from the construction of the Roman colosseum to the Holland Tunnel I believe creating for the future is the most meaningful way to spend my time.`,
+      `At NYU, I am apart of the GLASS (Global Leaders and Scholars in STEM) Program, a highly-coveted honors program for students to explore and apply a global-engineering persepective to their future endeavors.
+       Throughout my undergraduate journey, I have been fortunate to have incredible mentors who have provided me resources to pursue my goals.`,
+      `I hope this website serves as a resource for others and and my gratitude for those who helped me pursue my goals.`,
+    ],
+    currentProject: "Roebling",
+    currentProjectUrl: "",
+  },
+  experience: [
+    {
+      title: "Summer Associate: Software Engineering",
+      company: "Balyasny Asset Management",
+      year: "2023",
+      companyLink: "https://www.bamfunds.com",
+      desc: "Macro Technology Team",
+    },
+    {
+      title: "Founder and CEO",
+      company: "Roebling Research",
+      year: "2023",
+      companyLink: "",
+      desc: "Developing a global wealth management platform for every investor.",
+    },
+    {
+      title: "Commercial Modeling Intern",
+      company: "Vault Risk Management",
+      year: "2022",
+      companyLink: "https://vault.insurance",
+      desc: "Developed excel rating model for D&O and E&O insurance, including rating metrics and pricing tools under the commercial insurance team.",
+    },
+    {
+      title: "Software Engineering Intern",
+      company: "HIFI",
+      year: "2022",
+      companyLink: "https://hi.fi",
+      desc: "Venture-capital backed FinTech startup developing transparent financial transactions for the music industry. " +
+      " Used end-to-end testing and automation frameworks. Developed mobile UI application features for user authentication and accessibility. ",
+    },
+    {
+      title: "Student @ NYU Tandon School of Engineering",
+      company: "New York University.",
+      year: "2021",
+      companyLink: "https://nyu.edu",
+      desc: "Major in Applied Mathematics and Computer Science. GLASS Scholar (Awarded to top students in Tandon). Student Researcher @ NYU Visualization and Data" +
+      " Analytics Center. Hamiltonian Neural Network development and Dynamical Systems Researcher @ NYU Abu Dhabi SITE. Mentored by Prof. Claudio Silva and Prof. Nader Masmoudi.",
+    }
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1MpK__mvyhIN5XyqUIR2AKEUwT9SDChCU/view?usp=sharing",
+  socialLinks: {
+    linkedin: "https://www.linkedin.com/in/ruthvik-mukkamala-501139140/",
+    github: "https://github.com/ruthvikmukkamala",
+  },
+};
+
+export default userData;
